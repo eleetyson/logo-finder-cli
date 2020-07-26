a5d128cbf4a659c58ea3b3a1bb75978afd191d09
Ethan <eleetyson@gmail.com>
2020-07-26 15:51:39 -0400

    require clearbit gem

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 require 'pry'
 require 'httparty'
+require 'clearbit'
+
+Clearbit.key = '{sk_22070fd03165830dfc7c33a098991734}'
 
 require_relative '../lib/cli.rb'
 require_relative '../lib/finder.rb'
